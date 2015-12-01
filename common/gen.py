45c62ed91b66ca9af94626d74d2585c18e703bff
Denis Stepulenok <super.denis@gmail.com>
2015-12-01 23:51:11 +0300

    Save changes

--- common/gen.py
+++ common/gen.py
@@ -4,7 +4,7 @@ from string import Template
 from os import listdir
 from os.path import isfile, join, dirname, realpath
 
-#from BeautifulSoup import BeautifulSoup
+# from BeautifulSoup import BeautifulSoup
 from bs4 import BeautifulSoup
 
 import sys
@@ -13,17 +13,18 @@ if sys.version < '3':
     reload(sys)  # Reload does the trick!
     sys.setdefaultencoding('UTF-8')
 
+
 # Filter *.html files
 def check_files(curFile):
-    thisDir = dirname(realpath(curFile)) # __file__
+    thisDir = dirname(realpath(curFile))  # __file__
     allFiles = set(f for f in listdir(thisDir) if isfile(join(thisDir, f)) and f.endswith(".html"))
-    #allFiles.remove('index_template.html')
+    # allFiles.remove('index_template.html')
     allFiles.remove('index.html')
     print(allFiles)
 
 
 def ReadTemplate(template_fn):
-    return open('..\\common\\' + template_fn, 'r').read()
+    return open('..\\common\\' + template_fn, 'r', encoding='utf8').read()
 
 
 def GenFile(template, params, fn, overwrite=False):
@@ -38,11 +39,12 @@ def GenFile(template, params, fn, overwrite=False):
         return
     print('Generating "%s"...' % fn)
     if sys.version < '3':
-       f = open(fn, 'w')
+        f = open(fn, 'w')
     else:
-       f = open(fn, 'w', encoding = 'UTF-8')
+        f = open(fn, 'w', encoding='UTF-8')
     f.write(Template(template).safe_substitute(params))
     f.close()
+
 
 # Шаблон одной строки при генерации таблицы
 line_template = ReadTemplate("line_template.html")
@@ -62,6 +64,7 @@ class Session:
     def all_time(self):
         return self.all_theory + self.all_practice
 
+
 # Сумма по стоблцам
 session = Session()
 session_dist = Session()
@@ -76,19 +79,21 @@ class Theme():
         self.theme = theme
         self.title = title
 
+
 # Строка? Для Python2 и Python3
 def isString(arg):
-   if sys.version < '3':
-     return isinstance(arg, basestring)
-   else:
-     return isinstance(arg, str)
+    if sys.version < '3':
+        return isinstance(arg, basestring)
+    else:
+        return isinstance(arg, str)
+
 
 # Read from file
 def go(arg):
     global body, session, session_dist, tags
 
     if isString(arg):
-        filename = arg  
+        filename = arg
     elif isinstance(arg, Theme):
         d = {
             'theme': arg.theme,
@@ -101,24 +106,24 @@ def go(arg):
         filename, theory, practic, theory_dist, practic_dist = arg
 
     if sys.version < '3':
-       f = open(filename, "r")
+        f = open(filename, "r")
     else:
-       f = open(filename, "r", encoding = 'UTF-8')
+        f = open(filename, "r", encoding='UTF-8')
     html = f.read()
     f.close()
 
-    parsed_html = BeautifulSoup(html)
+    parsed_html = BeautifulSoup(html, "lxml")
     # Find hours and theme
-    #print parsed_html.body.find('h1').text
+    # print parsed_html.body.find('h1').text
 
     ul_tags = parsed_html.body.findAll(True, {'class': 'tags'})
-    print(ul_tags)
+    # print(str(ul_tags).encode('utf8').decode(sys.stdout.encoding))
     if len(ul_tags) == 0:
-      print(filename)
+        print(filename)
 
     for item in ul_tags[0].findAll('a'):
         tags.add(item.text)
-        #print u"Тема: ".encode('cp866'), item.text
+        # print u"Тема: ".encode('cp866'), item.text
 
     if isString(arg):
         theory = int(parsed_html.body.find('span', attrs={'class': 'theory'}).text)
@@ -143,22 +148,22 @@ def go(arg):
     session_dist.theme(theory_dist, practic_dist)
     # Заменяем все нули на &nbsp;
     if sys.version < '3':
-      for k, v in d.iteritems():
-        if v == 0:
-            d[k] = '&nbsp;'
+        for k, v in d.iteritems():
+            if v == 0:
+                d[k] = '&nbsp;'
     else:
-      for k, v in d.items(): 
-        if v == 0:
-            d[k] = '&nbsp;'
+        for k, v in d.items():
+            if v == 0:
+                d[k] = '&nbsp;'
 
     res = Template(line_template).safe_substitute(d)
     if sys.version < '3':
-       body += "\n" + res #.encode('utf-8')
+        body += "\n" + res  # .encode('utf-8')
     else:
-       body += "\n" + res
+        body += "\n" + res
 
 
-#print parsed_html.body.find('div', attrs={'class':'container'}).text
+# print parsed_html.body.find('div', attrs={'class':'container'}).text
 
 import color_console as cons
 
@@ -168,9 +173,9 @@ def Check(expected, actual, message):
         default_colors = cons.get_text_attr()
         default_bg = default_colors & 0x0070
         cons.set_text_attr(cons.FOREGROUND_RED | default_bg | cons.FOREGROUND_INTENSITY)
-        print ('ERROR:',)
+        print('ERROR:', )
         cons.set_text_attr(default_colors)
-        print (message % (expected, actual))
+        print(message % (expected, actual))
 
 
 # Генеририрование index.html для конкретной сессии
@@ -183,12 +188,12 @@ def GenerateIndex(title):
     themes = list(tags)
     themes.sort()
     # print (', '.join(themes)).encode("utf-8")
-    print ('Themes: %d' % len(themes))
+    print('Themes: %d' % len(themes))
 
     GenFile(ReadTemplate("index_template.html"),
             {
                 'title': title,
-                'body': body, # .encode("utf-8"),
+                'body': body,  # .encode("utf-8"),
                 'all_theory': session.all_theory,
                 'all_practic': session.all_practice,
                 'all_session': session.all_time(),
